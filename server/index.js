376366c9c78bf2939726a716c634bd57d15f5ca6
Sooraj <soorajsingh7505@gmail.com>
2023-09-20 17:35:07 +0530

    upload

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-          origin: 'http://localhost:3000',
+          origin: "http://localhost:3000", // Replace with the actual origin of your frontend
+          credentials: true, // Enable credentials (cookies, authorization headers, etc.)
 }));
 app.use(router);
 
