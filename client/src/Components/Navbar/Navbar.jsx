e5ba411915cc57dcf3b7be128138eab32ac03300
Sooraj <soorajsingh7505@gmail.com>
2023-10-06 15:56:49 +0530

    upload

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -3,13 +3,14 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Avatar from "@mui/material/Avatar";
 import { NavLink, useNavigate } from "react-router-dom";
-import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 
 import "./Navbar.css";
 import { ContextNavigate } from "../ContextProvider/Context";
 
 const Navbar = () => {
+  const [activeTab, setActiveTab] = useState("home");
+
   const history = useNavigate();
   const { userdata, setUserData } = useContext(ContextNavigate);
   // console.log(userdata);
@@ -96,89 +97,95 @@ const Navbar = () => {
       <div className="navbar">
         <AppBar className="appbar">
           <Toolbar>
-            <div className="tab">
-              <button className="btn btn-primary">
-                <NavLink
-                  to={"/home"}
-                  style={{
-                    color: "#fff",
-                    textDecoration: "none",
-                    fontSize: "25px",
-                  }}
-                >
-                  Home
-                </NavLink>
-              </button>
+            <div
+              className={`tab ${activeTab === "home" ? "active" : ""}`}
+              onClick={() => setActiveTab("home")}
+            >
+              <NavLink
+                to={"/home"}
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: "25px",
+                }}
+              >
+                Home
+              </NavLink>
             </div>
-            <div className="tab">
-              <button className="btn btn-primary">
-                <NavLink
-                  to={"/about"}
-                  style={{
-                    color: "#fff",
-                    textDecoration: "none",
-                    fontSize: "25px",
-                  }}
-                >
-                  About
-                </NavLink>
-              </button>
+            <div
+              className={`tab ${activeTab === "about" ? "active" : ""}`}
+              onClick={() => setActiveTab("about")}
+            >
+              <NavLink
+                to={"/about"}
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: "25px",
+                }}
+              >
+                About
+              </NavLink>
             </div>
-            <div className="tab">
-              <button className="btn btn-primary">
-                <NavLink
-                  to={"/service"}
-                  style={{
-                    color: "#fff",
-                    textDecoration: "none",
-                    fontSize: "25px",
-                  }}
-                >
-                  Service
-                </NavLink>
-              </button>
+            <div
+              className={`tab ${activeTab === "service" ? "active" : ""}`}
+              onClick={() => setActiveTab("service")}
+            >
+              <NavLink
+                to={"/service"}
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: "25px",
+                }}
+              >
+                Service
+              </NavLink>
             </div>
-            <div className="tab">
-              <button className="btn btn-primary">
-                <NavLink
-                  to={"/portfolio"}
-                  style={{
-                    color: "#fff",
-                    textDecoration: "none",
-                    fontSize: "25px",
-                  }}
-                >
-                  Portfolio
-                </NavLink>
-              </button>
+            <div
+              className={`tab ${activeTab === "portfolio" ? "active" : ""}`}
+              onClick={() => setActiveTab("portfolio")}
+            >
+              <NavLink
+                to={"/portfolio"}
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: "25px",
+                }}
+              >
+                Portfolio
+              </NavLink>
             </div>
-            <div className="tab">
-              <button className="btn btn-primary">
-                <NavLink
-                  to={"/contact"}
-                  style={{
-                    color: "#fff",
-                    textDecoration: "none",
-                    fontSize: "25px",
-                  }}
-                >
-                  Contact
-                </NavLink>
-              </button>
+            <div
+              className={`tab ${activeTab === "contact" ? "active" : ""}`}
+              onClick={() => setActiveTab("contact")}
+            >
+              <NavLink
+                to={"/contact"}
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: "25px",
+                }}
+              >
+                Contact
+              </NavLink>
             </div>
-            <div className="tab">
-              <button className="btn btn-primary">
-                <NavLink
-                  to={"/login"}
-                  style={{
-                    color: "#fff",
-                    textDecoration: "none",
-                    fontSize: "25px",
-                  }}
-                >
-                  Login
-                </NavLink>
-              </button>
+            <div
+              className={`tab ${activeTab === "login" ? "active" : ""}`}
+              onClick={() => setActiveTab("login")}
+            >
+              <NavLink
+                to={"/login"}
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: "25px",
+                }}
+              >
+                Login
+              </NavLink>
             </div>
             <div className="avatar">
               <Avatar className="avatar-main" onClick={handleAvatarClick}>
@@ -190,37 +197,59 @@ const Navbar = () => {
               </Avatar>
               {userdata ? (
                 userdata && (
-                  <Menu
+                  <div
                     id="manu"
+                    className="loginAvatarManu"
                     anchorEl={anchorEl}
                     open={Boolean(anchorEl)}
                     onClose={handleClose}
                   >
-                    <MenuItem onClick={() => history("/home")}>Home</MenuItem>
-                    <MenuItem onClick={() => history("/about")}>About</MenuItem>
-                    <MenuItem onClick={() => history("/service")}>
+                    <MenuItem onClick={() => history("/home")} className="item">
+                      Home
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => history("/about")}
+                      className="item"
+                    >
+                      About
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => history("/service")}
+                      className="item"
+                    >
                       Service
                     </MenuItem>
-                    <MenuItem onClick={() => history("/portfolio")}>
+                    <MenuItem
+                      onClick={() => history("/portfolio")}
+                      className="item"
+                    >
                       Portfolio
                     </MenuItem>
-                    <MenuItem onClick={() => history("/contact")}>
+                    <MenuItem
+                      onClick={() => history("/contact")}
+                      className="item"
+                    >
                       Contact
                     </MenuItem>
-                    <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
-                  </Menu>
+                    <MenuItem onClick={handleSignOut} className="item">
+                      Sign Out
+                    </MenuItem>
+                  </div>
                 )
               ) : (
-                <Menu
+                <div
+                  className="avatarManu"
                   anchorEl={anchorEl}
                   open={Boolean(anchorEl)}
                   onClose={handleClose}
                 >
-                  <MenuItem onClick={() => history("/home")}>Home</MenuItem>
-                  <MenuItem onClick={() => history("/contact")}>
+                  <MenuItem
+                    onClick={() => history("/contact")}
+                    className="item"
+                  >
                     Contact
                   </MenuItem>
-                </Menu>
+                </div>
               )}
             </div>
           </Toolbar>
